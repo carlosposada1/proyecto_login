4afb6948a6cd52d2f0d08b46f5cbf896f770e5ef
carlosposada1 <contactocarlosposada@gmail.com>
2023-02-02 11:30:09 -0500

    login

--- login.py
+++ login.py
@@ -2,24 +2,43 @@ import flet as ft
 from flet import(Column)
 
 def main(page: ft.Page):
-    page.title = "Bot BBVA"
+    page.title = "Bot Company"
     ft.Text(value="LIBERTY")
     page.vertical_alignment = ft.MainAxisAlignment.START
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.padding = 0
     page.bgcolor = "#A9D4D0"
     titulo = ft.Text(value="BOT ACTIVACION MOVIL", font_family="Arial")
-    user_gac = ft.TextField(label ="        USER GAC", width=320)
+    user_gac = ft.TextField(label ="        USER GAC", prefix_icon=ft.icons.PERSON, width=320)
     password_gac = ft.TextField(label="        PASSWORD GAC",  width=320)
     password_udb = ft.TextField(label="        PASSWORD UDB",  width=320)
     greetings = ft.Column()
-    subtitulo = ft.Text(value="© Atento RPA, Ing. Daniel Ríos")
+    subtitulo = ft.Text(value="© TecnoAndroid, Ing. Carlos Posada")
+    
+    
+    img = ft.Image(
+        src=f"/assets/tecno.PNG",
+        width=200,
+        height=110,
+        fit=ft.ImageFit.CONTAIN,)
+    border_radius=ft.border_radius.all(10),
+    images = ft.Row( wrap=False, scroll="always")
     
     c = ft.Container(
-        ft.Text(f"   BBVA", size=30, color="blue", italic=True, weight="bold"),
         
+            ft.Image(
+                
+                src=f"/assets/tecno.PNG",
+                border_radius=ft.border_radius.all(10),
+                width=320,
+                height=70,
+                fit=ft.ImageFit.NONE,
+            ),
+        
+        
+         
         width=1920,
-        height=60,
+        height=70,
         padding=0,
         alignment=ft.alignment.center_left,
         bgcolor="white",
@@ -30,16 +49,10 @@ def main(page: ft.Page):
     
     def btn_click(e):
         titulo = ""
-        user_gac.value = ""
-        password_gac.value = ""
-        password_udb.value = ""
+
         subtitulo = ""
         page.update()
         
-        
-    def toggle_icon_button(e):
-        e.control.selected = not e.control.selected
-        e.control.update()
     
    
     
@@ -47,9 +60,9 @@ def main(page: ft.Page):
         ft.Column(controls=[
             
             
-            ft.Text(f"BOT ACTIVACION MOVIL", size=14, color="black", font_family="Arial", weight="bold" ),
+            ft.Text(f"BOT ACTIVACION MOVIL", size=16, color="black", font_family="Arial", weight="bold" ),
             ft.Text(f"  ", size=5),
-            ft.TextField(label="        USER GAC",prefix_icon=ft.icons.PERSON, width=320),
+            ft.TextField(label="  USER GAC", prefix_icon=ft.icons.PERSON, width=320),
             ft.TextField(label="        PASSWORD GAC",  width=320),
             ft.TextField(label="        PASSWORD UDB",  width=320),
             
@@ -59,7 +72,7 @@ def main(page: ft.Page):
                 shape=ft.StadiumBorder(),
             ),
         ),
-            ft.Text(value="© Atento RPA, Ing. Daniel Ríos", weight="bold")
+            ft.Text(value="© TecnoAndroid, Ing. Carlos Posada", weight="bold")
     ],
                   alignment=ft.MainAxisAlignment.CENTER,
                   horizontal_alignment="center",
@@ -88,4 +101,5 @@ def main(page: ft.Page):
     
 
 
-ft.app(target=main)
+ft.app(target=main,
+       assets_dir="./assets")
